4cfd2cecbe7b39586bf472b9321086373ae6824f
Samuel Garneau <sgarneau@mirego.com>
2013-10-23 20:37:51 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ocnews::Application.config.secret_key_base = 'c7efcf102fa1ee7aa37de7dd116b41b3b45c7d7125cbb76bec055ae4ec129a44d2cdb094c3da0b1c927639ab7c1171312da61fdd90e9a3aadb219e63cc5383b4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ocnews::Application.config.session_store :cookie_store, key: '_ocnews_session'
