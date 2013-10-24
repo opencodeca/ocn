b282e4eef6da9441aa4748c543a4d0aeba59c082
Samuel Garneau <sgarneau@mirego.com>
2013-10-23 20:52:46 -0400

    Add choices and default configuration

--- Gemfile
+++ Gemfile
@@ -6,6 +6,9 @@ gem 'rails', '4.0.0'
 # Use pg as the database for Active Record
 gem 'pg'
 
+# Use choices for custom rails settings
+gem 'choices'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
 
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,10 @@ Bundler.require(:default, Rails.env)
 
 module Ocnews
   class Application < Rails::Application
+
+    # Load settings in config/settings.yml
+    config.from_file 'settings.yml'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
