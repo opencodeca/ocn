5efa4316ef4cdd7a3fbd47103f2c5970db4b422d
Samuel Garneau <sgarneau@mirego.com>
2013-10-23 22:49:05 -0400

    Add schema for links

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20131024013627) do
+
+  create_table "links", force: true do |t|
+    t.string   "title"
+    t.text     "url"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
