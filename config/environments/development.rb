79d51f33d06d4ec5099d29dc3450d543e2ca4383
Samuel Garneau <sgarneau@mirego.com>
2013-10-25 19:22:02 -0400

    Add device configuration and views

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,9 @@ Ocnews::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Mailer
+  config.action_mailer.default_url_options = {
+    :host => "#{Rails.configuration.protocol}#{Rails.configuration.domain}#{Rails.configuration.port}"
+  }
 end
