2f4286d619b96a251f79a8ab6a5d73a6da34288e
Samuel Garneau <sgarneau@mirego.com>
2013-10-25 17:48:03 -0400

    Add first links view

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Ocnews
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :fr
 
     # Canonical host
     if Rails.configuration.domain
---
+++ spec/models/link.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe Link do
+  describe :newest do
+  end
+end
