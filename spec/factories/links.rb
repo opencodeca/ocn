bf0671f4b55b17debb3f350289e393bebb4f82a9
Rémi Prévost <remi@exomel.com>
2013-11-04 17:05:04 -0500

    Remove unused spec files

--- spec/factories/links.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :link do
-    title "MyString"
-    url "MyText"
-    description "MyText"
-  end
-end
--- spec/models/link.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-
-describe Link do
-  describe :newest do
-  end
-end
