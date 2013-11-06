941e11a3ba43273a764c3ada55e0e8660431fbb7
Rémi Prévost <remi@exomel.com>
2013-11-05 21:01:08 -0500

    Add User@gravatar_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131106012342) do
+ActiveRecord::Schema.define(version: 20131106015153) do
 
   create_table "comments", force: true do |t|
     t.integer  "commentable_id"
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(version: 20131106012342) do
     t.string   "uid"
     t.string   "name"
     t.string   "username"
+    t.string   "gravatar_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
