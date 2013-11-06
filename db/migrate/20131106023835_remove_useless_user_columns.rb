53c9cd5fa022fff298c785f88fdace62f6779d3d
Rémi Prévost <remi@exomel.com>
2013-11-05 21:40:59 -0500

    Remove useless columns in User model

---
+++ db/migrate/20131106023835_remove_useless_user_columns.rb
@@ -0,0 +1,8 @@
+class RemoveUselessUserColumns < ActiveRecord::Migration
+  def change
+    remove_column :users, :encrypted_password
+    remove_column :users, :reset_password_token
+    remove_column :users, :reset_password_sent_at
+    remove_column :users, :remember_created_at
+  end
+end
