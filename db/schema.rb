d3dc9a2c02dcde384bac31581a792a80175dd8d5
Rémi Prévost <remi@exomel.com>
2013-11-05 22:05:13 -0500

    Remove reset_password_token index

--- db/schema.rb
+++ db/schema.rb
@@ -85,6 +85,5 @@ ActiveRecord::Schema.define(version: 20131106023835) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
