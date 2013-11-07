dfc4a4ce30f7dd51ce36b140bf1566ada602386c
Samuel Garneau <sgarneau@mirego.com>
2013-11-07 00:22:04 -0500

    Add is_presentator to User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131106042932) do
+ActiveRecord::Schema.define(version: 20131107051830) do
 
   create_table "comments", force: true do |t|
     t.integer  "commentable_id"
@@ -70,8 +70,8 @@ ActiveRecord::Schema.define(version: 20131106042932) do
   add_index "posts", ["slug"], name: "index_posts_on_slug", unique: true, using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",              default: "", null: false
-    t.integer  "sign_in_count",      default: 0,  null: false
+    t.string   "email",              default: "",    null: false
+    t.integer  "sign_in_count",      default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 20131106042932) do
     t.string   "name"
     t.string   "username"
     t.string   "gravatar_id"
+    t.boolean  "is_presentator",     default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
