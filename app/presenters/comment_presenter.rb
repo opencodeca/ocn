ce1807a556034c73982dd66f30dd7e71dea2e31e
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2013-11-06 23:25:44 -0500

    Add points_count presenter for comments

--- app/presenters/comment_presenter.rb
+++ app/presenters/comment_presenter.rb
@@ -3,4 +3,9 @@ class CommentPresenter < Bourgeois::Presenter
   def author_link
     view.link_to commenter.username, view.app_user_path(commenter)
   end
+
+  # Return the localized score
+  def points_count
+    view.t('.points_count', count: like_about.count)
+  end
 end
