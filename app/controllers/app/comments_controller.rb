11e47f2bf1e50cd0709f83e74031a52f783adf55
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2013-11-10 16:34:47 -0500

    Add comments create action

--- app/controllers/app/comments_controller.rb
+++ app/controllers/app/comments_controller.rb
@@ -2,6 +2,21 @@ module App
   class CommentsController < AppController
     before_action :fetch_comment, only: [:show, :like]
 
+    # POST /comments/new
+    def create
+      @comment = Comment.new(commenter: current_user)
+      @comment.assign_attributes(comment_params)
+
+      if @comment.save
+        url = :back
+        url = app_post_path(@comment.commentable) if @comment.commentable.is_a?(Post)
+
+        redirect_to url, notice: t('.notice')
+      else
+        render :new
+      end
+    end
+
     # POST /comments/:id/like
     def like
       current_user.express!(:like, @comment)
@@ -12,6 +27,11 @@ module App
 
     def fetch_comment
       @comment = Comment.find(params[:id])
+    end
+
+    def comment_params
+      permitted_parameters = [:comment, :commentable_id, :commentable_type]
+      params.require(:comment).permit(*permitted_parameters)
     end
   end
 end
