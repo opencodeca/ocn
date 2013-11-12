65784771e875a4b7cf3d0eb8a51ad32d0b5e51f8
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2013-11-11 22:15:09 -0500

    Move to postgresql

--- app/models/concerns/post_popular_sorting.rb
+++ app/models/concerns/post_popular_sorting.rb
@@ -6,9 +6,9 @@ module PostPopularSorting
       limit_updated_at = Time.now - 2.month
       rank_select = "
         posts.*,
-        /power(((unix_timestamp(utc_timestamp())-unix_timestamp(posts.created_at))/200),0.7)
-        ), 8) as rank
         round(((posts.comments_count + 3 * posts.like_emotions_count)
+        /power(((extract(epoch from now()) - extract(epoch from posts.created_at))/200),0.7)
+        )::numeric, 8) as rank
       ".gsub(/\s+/, " ").strip
 
       select(rank_select).where('updated_at >= ?', limit_updated_at).order('rank DESC')
