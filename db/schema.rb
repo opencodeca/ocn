ec9a10983ce62799c2b5363b826b0721a65e9f73
Samuel Garneau <sgarneau@mirego.com>
2013-11-13 00:52:40 -0500

    Add roles to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131113032314) do
+ActiveRecord::Schema.define(version: 20131113054114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 20131113032314) do
     t.string   "gravatar_id"
     t.boolean  "is_presentator",     default: false
     t.integer  "karma",              default: 0
+    t.integer  "roles_mask"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
