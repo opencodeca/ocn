70a82614486544ec157e2f5511d42dc81a872d27
Samuel Garneau <sgarneau@mirego.com>
2013-12-09 08:59:22 -0500

    Remove unique index on email for User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131205044354) do
+ActiveRecord::Schema.define(version: 20131209135425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,7 +97,5 @@ ActiveRecord::Schema.define(version: 20131205044354) do
     t.integer  "karma",              default: 0
     t.integer  "roles_mask"
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
